1a7f2aaa6f2038a95db85aae1743181201d475c5
jedwed <83894732+jedwed@users.noreply.github.com>
2023-01-09 10:22:15 +1100

    Dockerized react app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
   server: {
+    host: true,
     port: 3000,
   },
 });
