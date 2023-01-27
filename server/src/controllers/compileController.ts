7b295261e6b261c23510efc922c9f5c1f8f4f4bf
jedwed <83894732+jedwed@users.noreply.github.com>
2023-01-28 00:17:28 +1100

    How do I run a docker subprocess in node in docker?????

--- server/src/controllers/compileController.ts
+++ server/src/controllers/compileController.ts
@@ -1,8 +1,18 @@
+// import util from "util";
+// import path from "path";
 import { Request, Response } from "express";
+// import { exec } from "child_process";
 
-function compile(req: Request, res: Response) {
+// const execPromise = util.promisify(exec);
+
+async function compile(req: Request, res: Response) {
   console.log(req.body);
   res.status(200).json({ message: "Received code", code: req.body.code });
+  // const currDir = path.resolve(__dirname, "temp");
+  // const { stdout } = await execPromise(
+  //   `docker run --mount type=bind,src=${currDir}/temp,dst=/program -w /program test`
+  // );
+  // res.status(200).json({ stdout, currDir });
 }
 
 export { compile };
