8731f0a3e8f8143d758b10c787d0330dd1bd54fe
jedwed <83894732+jedwed@users.noreply.github.com>
2023-05-13 10:43:17 +1000

    Added loading state

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -1,28 +1,39 @@
-import { useContext } from "react";
-import axios from "axios";
-import { WrenchIcon, BugAntIcon } from "@heroicons/react/24/solid";
-import { DebuggerContext } from "context/DebuggerContext";
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { WrenchIcon, BugAntIcon } from '@heroicons/react/24/solid';
+import { DebuggerContext } from 'context/DebuggerContext';
 
 function Toolbar() {
   const { code, handleSetConsoleOutput, handleSetError } =
     useContext(DebuggerContext);
+  const [loading, setLoading] = useState(false);
   function handleCompile() {
+    setLoading(true);
     axios
-      .post("http://localhost:8000/compile", {
-        language: "c",
+      .post('http://localhost:8000/compile', {
+        language: 'c',
         code,
       })
-      .then((response) => handleSetConsoleOutput(response.data.stdout))
+      .then((response) => {
+        setLoading(false);
+        handleSetConsoleOutput(response.data.stdout);
+      })
       .catch((error) => {
         handleSetConsoleOutput(error.response.data.stderr);
         handleSetError(true);
+        setLoading(false);
       });
   }
 
   return (
     <div className="flex h-12 border-b border-gray-200">
       <button
-        className="bg-blue-400 hover:bg-blue-500 active:bg-blue-600 text-white rounded px-2 py-2 mx-2 my-2 inline-flex items-center"
+        className={`text-white rounded px-2 py-2 mx-2 my-2 inline-flex items-center ${
+          loading
+            ? 'bg-blue-200 cursor-not-allowed'
+            : 'bg-blue-400 hover:bg-blue-500 active:bg-blue-600 '
+        }`}
+        disabled={loading}
         onClick={handleCompile}
       >
         <WrenchIcon className="h-4 w-4" />
@@ -30,7 +41,12 @@ function Toolbar() {
       </button>
 
       <button
-        className="bg-blue-400 hover:bg-blue-500 active:bg-blue-600 text-white rounded px-2 py-2 mx-2 my-2 inline-flex items-center"
+        className={`text-white rounded px-2 py-2 mx-2 my-2 inline-flex items-center ${
+          loading
+            ? 'bg-blue-200 cursor-not-allowed'
+            : 'bg-blue-400 hover:bg-blue-500 active:bg-blue-600 '
+        }`}
+        disabled={loading}
         onClick={handleCompile}
       >
         <BugAntIcon className="h-4 w-4" />
