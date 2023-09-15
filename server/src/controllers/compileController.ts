3326c7bfe9edd51f55782015f1e60de346b147ec
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-15 22:46:23 +1000

    Create new directory for each compile request

--- server/src/controllers/compileController.ts
+++ server/src/controllers/compileController.ts
@@ -12,28 +12,28 @@ async function compile(req: Request, res: Response) {
   if (!req.body.code) {
     return res.status(400).json({ error: 'No code given' });
   }
-  const newFile = uuidv4();
-  await fs.writeFile(path.resolve(codeDir, `${newFile}.c`), req.body.code);
+  const newDir = path.resolve(codeDir, uuidv4());
+  await fs.mkdir(newDir);
+  await fs.writeFile(path.resolve(newDir, `main.c`), req.body.code);
 
   // const timer = setTimeout(() => {
   //   execPromise(`docker kill ${container}`);
   // }, 5000);
 
   try {
-    const { stdout } = await execPromise(
-      `gcc -ggdb ${newFile}.c -o ${newFile} && ./${newFile}`,
-      { cwd: codeDir }
-    );
-    const child = spawn('gdb', [newFile], { cwd: codeDir });
-    child.stdin.write(`source debug.py`);
+    const { stdout } = await execPromise(`gcc -ggdb main.c -o main && ./main`, {
+      cwd: newDir,
+    });
+    const child = spawn('gdb', ['main'], { cwd: newDir });
+    child.stdin.write(`source ${path.resolve(codeDir, 'debug.py')}`);
     child.stdin.end();
     await new Promise((resolve) => child.on('exit', resolve));
-    const debug = await fs.readFile(`${codeDir}/debug.json`, 'utf8');
+    const debug = await fs.readFile(path.resolve(newDir, 'debug.json'), 'utf8');
     // clearTimeout(timer);
-    execPromise(`rm ${newFile}.c ${newFile} debug.json`, { cwd: codeDir });
+    await fs.rm(newDir, { recursive: true, force: true });
     res.status(200).json({ stdout, debug: JSON.parse(debug) });
   } catch (error) {
-    execPromise(`rm -f ${newFile}.c ${newFile}`, { cwd: codeDir });
+    await fs.rm(newDir, { recursive: true, force: true });
     res.status(400).json(error);
   }
 }
