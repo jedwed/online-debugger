73c339feb8e3538e08c6a5d9b2128cb08eb8aff2
jedwed <83894732+jedwed@users.noreply.github.com>
2023-09-17 11:04:10 +1000

    Use marker to highlight currently highlighted line

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,5 +1,4 @@
 import AceEditor, { IMarker } from 'react-ace';
-import 'ace-builds/src-noconflict/theme-dracula';
 import 'ace-builds/src-noconflict/mode-c_cpp';
 
 function Editor({
@@ -11,27 +10,27 @@ function Editor({
   handleSetCode: (newCode: string) => void;
   currLine: number;
 }) {
-  const annotations = [
+  const markers: IMarker[] = [
     {
-      row: currLine - 1, // must be 0 based
-      text: 'Current line',
-      type: 'info',
+      startRow: currLine - 1,
+      startCol: 0,
+      endRow: currLine - 1,
+      endCol: 1,
+      className: 'marker',
+      type: 'fullLine',
     },
   ];
   return (
     <div className="h-[65%]">
       <AceEditor
-        // theme="dracula"
         mode="c_cpp"
         value={code}
         onChange={handleSetCode}
         height="100%"
-        // height="65%"
         width="100%"
         fontSize="15px"
         showPrintMargin={false}
-        // markers={markers}
-        annotations={annotations}
+        markers={markers}
       />
     </div>
   );
